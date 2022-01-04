73d91ebe73210b1b7cfd0a8e7f1af09e143187b6
Thomas Lee <tcl171@gmail.com>
2022-01-03 23:00:34 -0800

    making countMelds function. needs to be tested

--- server/modules/game.js
+++ server/modules/game.js
@@ -17,6 +17,55 @@ function handToString(hand) {
     return retString.slice(0, retString.length-1);
 }
 
+function countMeldsHelper(arr, i) {
+    if (i == arr.length) {
+        return 0;
+    }
+    if (arr[i] == 0) {
+        return countMeldsHelper(arr, i+1);
+    }
+    if (i > arr.length-3) { // Unable to find any more straights 
+        if (arr[i] == 3) { // Is pong
+            arr[i] = 0;
+            return 1 + countMeldsHelper(arr, i+1);
+        } else {
+            return countMeldsHelper(arr, i+1);
+        }
+    } else { // We know that arr[i] contains a number > 0
+        if (arr[i] < 3) { // Not possible to be a pong
+            if (arr[i + 1] > 1 && arr[i + 2] > 1) {
+                arr[i] -= 1;
+                arr[i+1] -= 1;
+                arr[i+2] -= 1;
+                return 1 + countMeldsHelper(arr, i); // Do not increment, could be more straights
+            } else {
+                return countMeldsHelper(arr, i+1);
+            }
+        } else {
+            let withPong = [...arr];
+            withPong[i] -= 3;
+            if (arr[i + 1] > 1 && arr[i + 2] > 1) { // Straight possible
+                arr[i] -= 1;
+                arr[i+1] -= 1;
+                arr[i+2] -= 1;
+                return Math.max(1+countMeldsHelper(withPong, i), 1 + countMeldsHelper(arr, i)); // Do not increment, could be more straights
+            } else {
+                return countMeldsHelper(arr, i+1);
+            }
+        }
+    }
+
+}
+
+/**
+ * Counts the highest number of melds in an array of tiles of the same suit.
+ * Do not use for honor tiles!
+ * @param {String[]} arr 
+ */
+function countMelds(arr) {
+    return countMeldsHelper(arr, 1);
+}
+
 /**
  * Checks if the hand is the Nine Gates. b, c, d, and h are arrays of tiles, with the first element
  * of each array being the sum of the other array elements.
@@ -41,7 +90,7 @@ function checkOrphans(b, c, d, h) {
         arr = h;
     }
     let found = false;
-    for (let i = 1; i < 10; i += 1) {
+    for (let i = 1; i < arr.length; i += 1) {
         if (arr[i] == 2) {
             found = true;
             break;
@@ -145,7 +194,55 @@ function checkGreatDragons(b, c, d, h) {
     if (!(h[1] == 3 && h[2] == 3 && h[3] == 3)) { // Check three dragon melds
         return false;
     }
+
+    /** Find eyes and remove them */
+    let arr = null;
+    if ((b[0]-2)%3 == 0) { // eyes are in here or no eyes at all
+        arr = b;
+    } else if ((c[0]-2)%3 == 0) {
+        arr = c;
+    } else if ((d[0]-2)%3 == 0) {
+        arr = d;
+    } else {
+        arr = h;
+    }
+    let found = false;
+    for (let i = 1; i < arr.length; i += 1) {
+        if (arr[i] == 2) {
+            found = true;
+            arr[0] -= 2;
+            arr[i] = 0; // REMOVE THE EYES!! Should only be one suit now.
+            break;
+        }
+    }
+    if (!found) { // If unable to find the eyes
+        return false;
+    }
+
+    // Check if the last meld is a wind
+    if (h[4] == 3 || h[5] == 3 || h[6] == 3 || h[7] == 3) {
+        return true;
+    }
+
+    // If the meld is not a wind...
+    if ((b[0] == 0) + (c[0] == 0) + (d[0] == 0) != 1) { // One suit
+        return false;
+    }
+    let arr = null;
+    if (b[0]>0) { // the meld must be in here
+        arr = b;
+    } else if (c[0]>0) {
+        arr = c;
+    } else{
+        arr = d;
+    }
+    for (let i = 1; i <= 7; i += 1) {
+        if (arr[i] == 3 || (arr[i]==1 && arr[i+1] == 1 && arr[i+2])) { // There is a pong or a straight
+            return true;
+        }
+    }
     
+    return false;
 }
 
 class Game {
